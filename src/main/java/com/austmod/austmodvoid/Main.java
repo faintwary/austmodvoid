2d224e387b90964a0bd1ae38c1a6ef2ee06407a5
faintwary <faintwary@github.com>
2020-06-15 21:04:53 -0400

    Prepared for release

--- src/main/java/com/austmod/austmodvoid/Main.java
+++ src/main/java/com/austmod/austmodvoid/Main.java
@@ -17,7 +17,7 @@ import org.apache.logging.log4j.Logger;
 public class Main
 {
     public static final String MODID = "austmodvoid";
-    public static final String VERSION = "0.3.1";
+    public static final String VERSION = "1.0.0";
     public static final String NAME = "AustMod Void";
 
     public static Logger logger;
