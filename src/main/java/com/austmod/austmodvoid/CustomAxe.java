e46a4041cac31b16428e5d6f4be3ee6456ecdf78
faintwary <faintwary@github.com>
2020-06-15 20:49:45 -0400

    Nerfed overpowered Austrone Axe

--- src/main/java/com/austmod/austmodvoid/CustomAxe.java
+++ src/main/java/com/austmod/austmodvoid/CustomAxe.java
@@ -6,7 +6,7 @@ import net.minecraft.item.ItemAxe;
 public class CustomAxe extends ItemAxe {
 
     public CustomAxe(ToolMaterial material, String name) {
-            super(material, material.getAttackDamage(), 1.0F);
+            super(material, material.getAttackDamage(), -2.6F);
             this.setRegistryName(name);
             this.setUnlocalizedName(name);
             this.setCreativeTab(CreativeTabs.TOOLS);
